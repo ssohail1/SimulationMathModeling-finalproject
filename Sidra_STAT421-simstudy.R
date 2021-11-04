65c4dd2fbce9315f53e0fc21bd70200c82df66e6
Sidra Sohail <80551186+ssohail1@users.noreply.github.com>
2021-11-04 09:57:13 -0500

    Include the suscept$D variable
    including suscept$D variable in plotting and simulating

--- Sidra_STAT421-simstudy.R
+++ Sidra_STAT421-simstudy.R
@@ -2,13 +2,13 @@
 N <- 10000
 
 # Prob S to I
-alpha <- 0.005
+alpha <- 0.05
 
 # Prob I to R
-beta <- 0.4
+beta <- 0.004
 
 # Prob R to D
-d <- 0.05
+d <- 0.0005
 
 suscept <- list() # list with SIRD
 suscept$S[1] <- N
@@ -20,26 +20,27 @@ t_1 <- 1
 
 while (suscept$R[t_1] < N) {
   #while (TRUE) { # only when running a function i.e. while true runs until return a value i.e. in a function
-    suscept$S[t_1 + 1] <- rbinom(1, suscept$S[t_1], (1 - alpha)^(suscept$I[t_1]))
-    suscept$R[t_1 + 1] <- suscept$R[t_1] + rbinom(1, suscept$I[t_1], beta)
-    suscept$I[t_1 + 1] <- N - suscept$S[t_1 + 1] - suscept$R[t_1 + 1]
-   # suscept$D[t_1 + 1] <- suscept$D[t_1] + rbinom(1, suscept$R[t_1], d)
-    
-    t_1 <- t_1 + 1
-    
-    # # part where I am trying to restore/add all the recovered (R) to susceptible pop (S)
-    # if (suscept$S == 0) {
-    #   while (TRUE) {
-    #     suscept$S[t_1] <- suscept$R[t_1]
-    #     suscept$R[t_1] <- 0
-      }
-    #}
+  suscept$S[t_1 + 1] <- rbinom(1, suscept$S[t_1], (1 - alpha)^(suscept$I[t_1]))
+  suscept$R[t_1 + 1] <- suscept$R[t_1] + rbinom(1, suscept$I[t_1], beta)
+  suscept$I[t_1 + 1] <- N - suscept$S[t_1 + 1] - suscept$R[t_1 + 1]
+  suscept$D[t_1 + 1] <- suscept$D[t_1] + rbinom(1, suscept$R[t_1], d)
+  
+  t_1 <- t_1 + 1
+  
+  # # part where I am trying to restore/add all the recovered (R) to susceptible pop (S)
+  # if (suscept$S == 0) {
+  #   while (TRUE) {
+  #     suscept$S[t_1] <- suscept$R[t_1]
+  #     suscept$R[t_1] <- 0
+}
+#}
 #   }
 # }
 
 plot(suscept$S,type = "l",col = "green")
 points(suscept$I,type = "l", col = "red")
 points(suscept$R,type = "l", col = "blue")
+points(suscept$D,type = "l", col = "black")
 
 
 
