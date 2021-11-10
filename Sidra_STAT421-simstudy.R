95f00a28d11649fbe714fe9982f16004317a54d1
Sidra Sohail <80551186+ssohail1@users.noreply.github.com>
2021-11-10 09:33:27 -0600

    Add probability of R to S
    Add probability of going from recovered back to susceptible

--- Sidra_STAT421-simstudy.R
+++ Sidra_STAT421-simstudy.R
@@ -2,19 +2,26 @@
 N <- 10000
 
 # Prob S to I
-alpha <- 0.05
+alpha <- 0.0002
 
-# Prob I to R
-beta <- 0.004
+# Prob I to R #infects quickly
+beta <- 0.03
 
-# Prob R to D
-d <- 0.0005
+# Prob R to D # is not as deadly
+d <- 0.001
 
-suscept <- list() # list with SIRD
+# Prob R to S # can be quickly infected again
+rs <- 0.09
+
+
+SIRDS <- function(StoIprob,ItoRprob,RtoDprob,RtoSprob,Npopul) {
+  
+suscept <- list() # list with SIRDS
 suscept$S[1] <- N
 suscept$I[1] <- 1
 suscept$R[1] <- 0
 suscept$D[1] <- 0
+suscept$RS[1] <- 0
 
 t_1 <- 1
 
@@ -24,7 +31,7 @@ while (suscept$R[t_1] < N) {
   suscept$R[t_1 + 1] <- suscept$R[t_1] + rbinom(1, suscept$I[t_1], beta)
   suscept$I[t_1 + 1] <- N - suscept$S[t_1 + 1] - suscept$R[t_1 + 1]
   suscept$D[t_1 + 1] <- suscept$D[t_1] + rbinom(1, suscept$R[t_1], d)
-  
+  suscept$RS[t_1 + 1] <- suscept$S[t_1] + rbinom(1, suscept$R[t_1], rs)
   t_1 <- t_1 + 1
   
   # # part where I am trying to restore/add all the recovered (R) to susceptible pop (S)
@@ -33,43 +40,15 @@ while (suscept$R[t_1] < N) {
   #     suscept$S[t_1] <- suscept$R[t_1]
   #     suscept$R[t_1] <- 0
 }
-#}
+return(list(sirds = suscept, t_ = length(suscept$S), t_1))
+}
 #   }
 # }
 
-plot(suscept$S,type = "l",col = "green")
-points(suscept$I,type = "l", col = "red")
-points(suscept$R,type = "l", col = "blue")
-points(suscept$D,type = "l", col = "black")
+sirdsfxn <- SIRDS(alpha,beta,d,rs,N)
 
-
-
-
-
-# for (i in suscept$R) {
-#   print(i)
-#   suscept$D[i] <- rbinom(1, suscept$R[i + 1], d)
-# }
-
-
-
-if (suscept$R[t_1] == (N + 1)) {
-  suscept$S[(length(suscept$S))] <- suscept$R[(length(suscept$R))]
-  suscept$R[(length(suscept$R))] <- 0
-}
-# t_1 <- t+1
-suscept$I[(length(suscept$I))] <- 1
-
-while (suscept$R[(length(suscept$R))] < (N + 1)) {
-  suscept$S[t_1 + 1] <- rbinom(1, suscept$S[t_1], (1 - alpha) ^ (suscept$I[t_1]))
-  
-  suscept$R[t_1 + 1] <- suscept$R[t_1] + rbinom(1, suscept$I[t_1], beta)
-  
-  suscept$I[t_1 + 1] <- N + 1 - suscept$S[t_1 + 1] - suscept$R[t_1 + 1]
-  
-  t_1 <- t_1 + 1
-}
-
-# suscept$S[(length(suscept$S))] <- suscept$R[(length(suscept$R))]
-suscept
-t
+plot(sirdsfxn$sirds$S,type = "l",col = "green")
+points(sirdsfxn$sirds$I,type = "l", col = "red")
+points(sirdsfxn$sirds$R,type = "l", col = "blue")
+points(sirdsfxn$sirds$D,type = "l", col = "black")
+points(sirdsfxn$sirds$RS,type = "l", col = "pink")
