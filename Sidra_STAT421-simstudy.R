e8c451d4079671aea714f1d1af929e6b31b43ad1
Sidra Sohail <80551186+ssohail1@users.noreply.github.com>
2021-11-22 09:09:10 -0600

    update sim study with multiple probabilities

--- Sidra_STAT421-simstudy.R
+++ Sidra_STAT421-simstudy.R
@@ -1,74 +1,84 @@
-# pop size
-N <- 10000
-
-# Prob S to I
-alpha <- 0.0005
-
-# Prob I to R #infects quickly
-beta <- 0.07
-
-# Prob R to D # is deadly
-d <- 0.09
-
-# Prob R to S # can be quickly susceptible again
-rs <- 0.4
-
-
 # # pop size
 # N <- 10000
 # 
 # # Prob S to I
-# alpha <- c(0.0005,0.03,0.008,0.01)
+# alpha <- 0.0005
 # 
 # # Prob I to R #infects quickly
-# beta <- c(0.07,0.0001,0.02,0.09)
+# beta <- 0.07
 # 
 # # Prob R to D # is deadly
-# d <- c(0.09,0.0007,0.002,0.05)
+# d <- 0.09
 # 
-# # Prob R to S # can be quickly infected again
-# rs <- c(0.04,0.08,0.0005,0.01)
+# # Prob R to S # can be quickly susceptible again
+# rs <- 0.4
 
+# probabilities as function of age group - going from 0-12, 12-25, 25-60, 60+
+# # pop size
+  N <- 10000
+# 
+# # Prob S to I
+ alpha <- c(0.05,0.08,0.2,0.5)
+# 
+# # Prob I to R 
+ beta <- c(0.2,0.3,0.07,0.03)
+# 
+# # Prob R to D
+ d <- c(0.005,0.004,0.02,0.08)
+# 
+# # Prob R to S 
+ rs <- c(0.04,0.04,0.04,0.04)
+
+ SIRDS <- function(alpha,beta,d,rs,N) {
+   
+   suscept <- list() # list with SIRDS
+   suscept$S[1] <- N
+   suscept$I[1] <- 1
+   suscept$R[1] <- 0
+   suscept$D[1] <- 0
+   suscept$RS[1] <- 0
+   
+   t_1 <- 1
+   
+   while (suscept$R[t_1] < N) {
+     #while (TRUE) { # only when running a function i.e. while true runs until return a value i.e. in a function
+     suscept$S[t_1 + 1] <- rbinom(1, suscept$S[t_1], (1 - alpha)^(suscept$I[t_1]))
+     suscept$R[t_1 + 1] <- suscept$R[t_1] + rbinom(1, suscept$I[t_1], beta)
+     suscept$I[t_1 + 1] <- N - suscept$S[t_1 + 1] - suscept$R[t_1 + 1]
+     suscept$D[t_1 + 1] <- suscept$D[t_1] + rbinom(1, suscept$R[t_1], d)
+     suscept$RS[t_1 + 1] <- suscept$S[t_1] + rbinom(1, suscept$R[t_1], rs)
+     t_1 <- t_1 + 1
+     
+     # # part where I am trying to restore/add all the recovered (R) to susceptible pop (S)
+     # if (suscept$S == 0) {
+     #   while (TRUE) {
+     #     suscept$S[t_1] <- suscept$R[t_1]
+     #     suscept$R[t_1] <- 0
+   }
+   return(list(sirds = suscept, t_ = length(suscept$S)))
+ }
+ 
+ 
 storeans <- c()
 for (i in alpha) {
   for (j in beta) {
     for (m in d) {
       for (a in rs) {
-        storeans <- c(SIRDS(i,j,m,a,N),storeans)
+        ans <- SIRDS(i,j,m,a,N)
+        storeans <- c(ans,storeans)
       }
     }
   }
 }
-SIRDS <- function(StoIprob,ItoRprob,RtoDprob,RtoSprob,Npopul) {
-  
-  suscept <- list() # list with SIRDS
-  suscept$S[1] <- N
-  suscept$I[1] <- 1
-  suscept$R[1] <- 0
-  suscept$D[1] <- 0
-  suscept$RS[1] <- 0
-  
-  t__1 <- 1
-  
-  while (suscept$R[t_1] < N) {
-    #while (TRUE) { # only when running a function i.e. while true runs until return a value i.e. in a function
-    suscept$S[t_1 + 1] <- rbinom(1, suscept$S[t_1], (1 - alpha)^(suscept$I[t_1]))
-    suscept$R[t_1 + 1] <- suscept$R[t_1] + rbinom(1, suscept$I[t_1], beta)
-    suscept$I[t_1 + 1] <- N - suscept$S[t_1 + 1] - suscept$R[t_1 + 1]
-    suscept$D[t_1 + 1] <- suscept$D[t_1] + rbinom(1, suscept$R[t_1], d)
-    suscept$RS[t_1 + 1] <- suscept$S[t_1] + rbinom(1, suscept$R[t_1], rs)
-    t_1 <- t_1 + 1
-    
-    # # part where I am trying to restore/add all the recovered (R) to susceptible pop (S)
-    # if (suscept$S == 0) {
-    #   while (TRUE) {
-    #     suscept$S[t_1] <- suscept$R[t_1]
-    #     suscept$R[t_1] <- 0
-  }
-  return(list(sirds = suscept, t_ = length(suscept$S), t_1))
+oddnum <- seq(1,512,2)
+for (i in oddnum) {
+  plot(storeans[i]$S,type = "l",col = "green")
+  points(storeans[i]$I,type = "l", col = "red")
+  points(storeans[i]$R,type = "l", col = "blue")
+  points(storeans[i]$D,type = "l", col = "black")
+  points(storeans[i]$RS,type = "l", col = "pink")
 }
-#   }
-# }
+
 
 sirdsfxn <- SIRDS(alpha,beta,d,rs,N)
 
